027f81beb1884d0c431c65ec8a9fa9181133bcbc
Campbell Tajwane J <n01088564@nj-00115-007.mediastudies.ca>
2018-10-29 13:12:37 -0400

    button functions started

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var numberOnScreen:Double = 0
+    @IBOutlet weak var label: UILabel!
+    @IBAction func numbers(_ sender: UIButton) {
+        label.text = label.text! + String(sender.tag-1)
+        numberOnScreen = Double(label.text!)!
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
